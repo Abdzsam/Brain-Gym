a370ff14f82d7b99ac365e2b56ab80d7281e3567
Abdul Samad <abdulsamad@dal.ca>
2025-10-13 21:19:49 -0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (91.02%) - LeetSync

---
+++ Easy/100-same-tree/same-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return [None]
+            return [node.val] + dfs(node.left) + dfs(node.right)
+
+        tarrP = dfs(p)
+        tarrQ = dfs(q)
+
+        return tarrP == tarrQ
